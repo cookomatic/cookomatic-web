abc2c2cc509dce47ace7e9624af20cd41fb7aab2
Ryan Hefner <rlhefner@ncsu.edu>
2017-01-20 22:18:39 -0500

    Fix client-secret.json path

--- ci/install.sh
+++ ci/install.sh
@@ -15,7 +15,7 @@ fi
 
 echo -e "\n## Authenticating to Google Cloud Platform"
 # Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
-openssl aes-256-cbc -K $encrypted_781b2254b6c6_key -iv $encrypted_781b2254b6c6_iv -in client-secret.json.enc -out client-secret.json -d
+openssl aes-256-cbc -K $encrypted_781b2254b6c6_key -iv $encrypted_781b2254b6c6_iv -in ./ci/client-secret.json.enc -out ./ci/client-secret.json -d
 
 # Here we use the decrypted service account credentials to authenticate the command line tool
 gcloud auth activate-service-account --key-file ./ci/client-secret.json
