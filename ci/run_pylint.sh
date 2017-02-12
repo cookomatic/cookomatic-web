77ef0b7412dab21027ac695443350f7514205c28
Ryan Hefner <rhefner1@gmail.com>
2017-02-11 20:55:25 -0500

    Change Flask app variable to lowercase (#9)

--- ci/run_pylint.sh
+++ ci/run_pylint.sh
@@ -3,6 +3,6 @@
 if [[ $RUN_PYLINT == true ]]
 then
     echo -e "\n### Running pylint"
-    pylint cookomatic_web --ignore tests --disable=no-member --disable too-few-public-methods --disable unused-argument
+    pylint cookomatic_web --ignore tests --disable=no-member --disable too-few-public-methods --disable unused-argument --disable invalid-name
     pylint cookomatic_web/tests --disable=no-member --disable too-few-public-methods --disable unused-argument --disable missing-docstring --disable protected-access --disable duplicate-code
 fi
--- cookomatic_web/http_handler.py
+++ cookomatic_web/http_handler.py
@@ -3,25 +3,25 @@
 import flask
 from flask import render_template
 
-APP = flask.Flask(__name__)
-APP.config['DEBUG'] = True
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
 
 SUCCESS = ('', 200)
 
 
-@APP.route('/ping')
+@app.route('/ping')
 def ping():
     """Sample web handler to see if the server is alive."""
     return 'pong'
 
 
-@APP.route('/')
+@app.route('/')
 def home_page():
     """Sample web handler to see if the server is alive."""
     return render_template('index.html')
 
 
-@APP.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(error):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL. Error msg: %s' % error, 404
