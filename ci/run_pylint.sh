08a9fcce13d273b6b95eb3d3211f0d95f217a231
Ryan Hefner <rhefner1@gmail.com>
2017-02-14 13:18:27 -0500

    Fix PYTHONPATH when running tests and pylint (#14)

--- ci/run_pylint.sh
+++ ci/run_pylint.sh
@@ -2,6 +2,9 @@
 
 if [[ $RUN_PYLINT == true ]]
 then
+    export PYTHONPATH="${PYTHONPATH}:$(realpath ~/google-cloud-sdk/platform/google_appengine)"
+    export PYTHONPATH="${PYTHONPATH}:$(realpath ~/google-cloud-sdk/platform/google_appengine/lib/yaml/lib)"
+
     echo -e "\n### Running pylint"
     pylint cookomatic_web --ignore tests --disable=no-member --disable too-few-public-methods --disable unused-argument --disable invalid-name
     pylint cookomatic_web/tests --disable=no-member --disable too-few-public-methods --disable unused-argument --disable missing-docstring --disable protected-access --disable duplicate-code
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -2,6 +2,9 @@
 
 if [[ $RUN_TESTS == true ]]
 then
+    export PYTHONPATH="${PYTHONPATH}:$(realpath ~/google-cloud-sdk/platform/google_appengine)"
+    export PYTHONPATH="${PYTHONPATH}:$(realpath ~/google-cloud-sdk/platform/google_appengine/lib/yaml/lib)"
+
     echo -e "\n## Running unit tests"
     nosetests  --with-coverage --cover-package=cookomatic_web --exclude-dir=lib
 fi
